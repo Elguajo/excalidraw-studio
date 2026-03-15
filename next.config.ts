359c8fefd5f99b98db300265fbe64ac253d32b75
Anmol Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2026-03-15 23:14:02 +0530

    chore: setup config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  transpilePackages: ["@excalidraw/excalidraw"],
+  serverExternalPackages: ["@copilotkit/runtime"],
 };
 
 export default nextConfig;
